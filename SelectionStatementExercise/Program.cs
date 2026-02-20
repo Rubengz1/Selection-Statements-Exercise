4219d2418e17650a955d7eeb65dfccbb310463c8
Ruben Gonzalez <rubengz23@yahoo.com>
2026-02-19 22:07:13 -0800

    Assignment 1 save

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var favNumber = 23;
+            
+            Console.WriteLine("Try to guess my favorite number, 1 through 100");
+            var userGuess = int.Parse(Console.ReadLine());
+
+            if(userGuess < favNumber)
+            {
+                Console.WriteLine("Too Low!");
+            }
+            else if (userGuess > favNumber)
+            {
+                Console.WriteLine("Too high.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!!!");
+            }
         }
     }
 }
